c8066a42b10fc1ff52ed6a46ac9acf2467e604dc
Jordan Stewart <81133588+JSordan22@users.noreply.github.com>
2024-12-08 14:53:55 -0500

    Made changing theme

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let button = document.querySelector(".sumbit-btn");
 let pass = document.getElementById("pass")
 let confPass = document.getElementById("confPass");
 let errMsg = document.getElementById("errMsg");
+let background = document.querySelector(".background");
+let root = document.documentElement;
 
 button.addEventListener("click", () => {
     if(pass.value != confPass.value) {
@@ -14,3 +16,22 @@ button.addEventListener("click", () => {
         errMsg.style.display = "none";
     }
 });
+
+let objs = {
+    bronze: { url: "Images/bronze.gif", color: "#937344"},
+    brown: { url: "Images/brown.gif", color: "#794633"},
+    green: { url: "Images/green.gif", color: "#697973"},
+    purple: { url: "Images/purple.gif", color: "#7A74B0"}
+}
+
+function selectTheme() {
+    let rndm = Math.floor(Math.random() * 4);
+    let keys = Object.keys(objs);
+    let rndmKey = objs[keys[rndm]]
+
+    background.style.backgroundImage = `url(${rndmKey.url})`;
+    root.style.setProperty('--mouse-y', rndmKey.color);;
+
+}
+
+window.addEventListener("load", selectTheme());
